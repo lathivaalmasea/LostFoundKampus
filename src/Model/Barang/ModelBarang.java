f660520aca0d4290ec0e9f74429d1b1735796512
lathiva almasea <lathivaalmasea@gmail.com>
2026-05-22 12:57:27 +0700

    update ModelBarang

--- src/Model/Barang/ModelBarang.java
+++ src/Model/Barang/ModelBarang.java
@@ -9,5 +9,69 @@ package Model.Barang;
  * @author Ivaa
  */
 public class ModelBarang {
-    
+    public class ModelBarang {
+
+    private int id;
+    private String namaBarang;
+    private String kategori;
+    private String deskripsi;
+    private String lokasi;
+    private String status;
+    private String tanggal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
 }
